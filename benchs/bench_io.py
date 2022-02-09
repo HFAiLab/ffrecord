52d81bacd06fdae9ceac748cd36fb95003d93660
Chengqi Deng <checkdeng0903@gmail.com>
2022-02-09 15:34:39 +0800

    Update bench_io.py

--- benchs/bench_io.py
+++ benchs/bench_io.py
@@ -1,12 +1,14 @@
-import sys
+import hf_env
+hf_env.set_env('202105')
+
 import time
 from pathlib import Path
 import matplotlib.pyplot as plt
 import multiprocessing as mp
 from multiprocessing import Process
+from tqdm import trange
 
 from ffrecord import FileWriter, FileReader
-from ffrecord.fileio import FileReader as PyFileReader
 
 
 def read_chunk(reader, q):
@@ -47,7 +49,7 @@ def bench_read(implem, Reader, fname, n, bs, nprocs):
         procs[i].join()
 
     t_read = time.time() - t0
-    print(f'{implem} read: {t_read}')
+    print(f'{implem} read: {t_read} s')
 
     return t_read
 
@@ -58,13 +60,13 @@ def main():
     tmp_dir = '/private_dataset'
     nprocs = 128
     n = 100000
-    fname = tmp_dir + f'/test_ss_{sample_size}'
+    fname = tmp_dir + f'/test_ss_{sample_size}.ffr'
 
     if not Path(fname).exists():
         writer = FileWriter(fname, n)
 
         t0 = time.time()
-        for i in range(n):
+        for i in trange(n):
             writer.write_one(data)
         t_write = time.time() - t0
         writer.close()
@@ -75,13 +77,11 @@ def main():
     print(f'Reading {size} GB from {fname}')
     batch_sizes = [64, 80, 96, 112, 128]
 
-    t_cpp, t_python = [], []
+    t_cpp = []
     for bs in batch_sizes:
-        t_python.append(bench_read('python', PyFileReader, fname, n, bs, nprocs))
         t_cpp.append(bench_read('cpp', FileReader, fname, n, bs, nprocs))
 
-    plt.plot(batch_sizes, [size / b for b in t_cpp], label='C++ read')
-    plt.plot(batch_sizes, [size / b for b in t_python], label='Python read')
+    plt.plot(batch_sizes, [size / b for b in t_cpp], label='read')
     plt.title(f'Read, nprocs {nprocs}, sample_size {sample_size}')
     plt.xlabel('batch size')
     plt.ylabel('GB/s')
