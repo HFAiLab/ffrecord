5579f2106e1fcf768e7c23f1d418f339d0a49bc7
Chengqi Deng <checkdeng0903@gmail.com>
2022-10-11 09:27:58 +0800

    Sync with master

--- tests/test_pl.py
+++ tests/test_pl.py
@@ -1,13 +1,15 @@
 import numpy as np
 import torch
 import torch.nn as nn
-import pytorch_lightning as pl
 import unittest
-from hfai.pl import HFAIEnvironment
 
 import ffrecord
 from ffrecord.torch import DataLoader, Dataset
 print(ffrecord.__version__, ffrecord)
+
+import os
+env = os.environ["HF_ENV_NAME"]
+exclude_envs = ["py3-202105", "py38-202207"]
 
 
 class ToyDataset(Dataset):
@@ -40,33 +42,36 @@ class ToyModel(nn.Module):
         return output
 
 
-class ToyNetModule(pl.LightningModule):
-    def __init__(self):
-        super().__init__()
-        self.model = ToyModel()
-        self.criterion = nn.MSELoss()
-
-    def forward(self, batch):
-        return self.model(batch)
-
-    def training_step(self, batch, batch_idx):
-        data, labels = batch
-        output = self(data)
-        loss = self.criterion(output, labels)
-        return loss
-
-    def configure_optimizers(self, lr=1e-3):
-        optimizer = torch.optim.AdamW(params=self.model.parameters(), lr=lr)
-        return optimizer
-
-    def train_dataloader(self, batch_size=1, num_workers=2):
-        traindataloader = DataLoader(ToyDataset(), batch_size=batch_size, num_workers=num_workers, skippable=False)
-        return traindataloader
-
-
 class TestDDPHF(unittest.TestCase):
 
+    @unittest.skipIf(env in exclude_envs, f"env {env} is not supported")
     def test_ddp_hf(self):
+        import pytorch_lightning as pl
+        from hfai.pl import HFAIEnvironment
+
+        class ToyNetModule(pl.LightningModule):
+            def __init__(self):
+                super().__init__()
+                self.model = ToyModel()
+                self.criterion = nn.MSELoss()
+
+            def forward(self, batch):
+                return self.model(batch)
+
+            def training_step(self, batch, batch_idx):
+                data, labels = batch
+                output = self(data)
+                loss = self.criterion(output, labels)
+                return loss
+
+            def configure_optimizers(self, lr=1e-3):
+                optimizer = torch.optim.AdamW(params=self.model.parameters(), lr=lr)
+                return optimizer
+
+            def train_dataloader(self, batch_size=1, num_workers=2):
+                traindataloader = DataLoader(ToyDataset(), batch_size=batch_size, num_workers=num_workers, skippable=False)
+                return traindataloader
+
         trainer = pl.Trainer(
             max_epochs=3,
             accelerator="gpu",
